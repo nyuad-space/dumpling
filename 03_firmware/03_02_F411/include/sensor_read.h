99ddd7ddbd09d141ac880cc7907df351e60b3893
Seoyoon Jung <sj4260@nyu.edu>
2025-06-08 18:42:20 -0500

    sensor_read better format

--- 03_firmware/03_02_F411/include/sensor_read.h
+++ 03_firmware/03_02_F411/include/sensor_read.h
@@ -19,7 +19,7 @@ extern float lsm6ds_temp_read;
 // DPS310
 extern float dps310_temp_read;
 extern float dps310_press_read;
-//BMI088
+// BMI088
 extern float bmi088_accel_x_read;
 extern float bmi088_accel_y_read;
 extern float bmi088_accel_z_read;
@@ -27,15 +27,15 @@ extern float bmi088_gyro_x_read;
 extern float bmi088_gyro_y_read;
 extern float bmi088_gyro_z_read;
 extern float bmi088_temp_read;
-//BMP390
+// BMP390
 extern float bmp390_temp_read;
 extern float bmp390_press_read;
 extern float bmp390_alt_read;
-//LIS2MDL
+// LIS2MDL
 extern float lis2mdl_mag_x_read;
 extern float lis2mdl_mag_y_read;
 extern float lis2mdl_mag_z_read;
-//HDC302
+// HDC302
 extern float hdc302_hum_read;
 extern float hdc302_temp_read;
 
