ef8ed2531cde133a5b235437ae4956a198e136a4
Seoyoon Jung <sj4260@nyu.edu>
2025-06-07 20:42:19 -0500

    reading lsm6

--- 03_firmware/03_02_F411/src/sensor_read.cpp
+++ 03_firmware/03_02_F411/src/sensor_read.cpp
@@ -8,11 +8,26 @@ void readSensor(SensorType sensorType)
     switch (sensorType)
     {
     case SENSOR_LSM6DS_ACCEL_GYRO:
+        // NOTE: remove below line when sensor_config.cpp is resolved
+        lsm6ds_accel_gyro.begin_SPI(LSM6DS_CS, &SENSOR_SPI);
+
+        // Read sensor data
+        sensors_event_t accel_event, gyro_event, temp_event;
+        dps310_baro_accel.getEvents(&accel_event, &gyro_event, &temp_event);
+
+        // Display on Serial
+        Serial.print(millis());
+        Serial.print("ms, T: ");
+        Serial.print(accel_event.acceleration, 3);
+        Serial.print("*C, P: ");
+        Serial.print(gyro_event.gyro, 3);
+        Serial.println("hPa");
+
         break;
 
     case SENSOR_DPS310_BARO_TEMP:
 
-        // remove below line when sensor_config.cpp is resolved
+        // NOTE: remove below line when sensor_config.cpp is resolved
         dps310_baro_temp.begin_SPI(DPS310_CS, &SENSOR_SPI);
 
         // true if new data available to read
@@ -21,20 +36,16 @@ void readSensor(SensorType sensorType)
             delay(100);
             return;
         }
-
         // Read sensor data
         sensors_event_t temp_event, pressure_event;
         dps310_baro_temp.getEvents(&temp_event, &pressure_event);
-        float T = temp_event.temperature;
-        float P = pressure_event.pressure;
 
         // Display on Serial
-
         Serial.print(millis());
         Serial.print("ms, T: ");
-        Serial.print(T, 3);
+        Serial.print(temp_event.temperature, 3);
         Serial.print("*C, P: ");
-        Serial.print(P, 3);
+        Serial.print(pressure_event.pressure, 3);
         Serial.println("hPa");
 
         // into SPI Packet
