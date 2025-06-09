c41f476bc3801ff9b26983c8c2ff4e0d68cca30d
Seoyoon Jung <sj4260@nyu.edu>
2025-06-08 23:29:36 -0500

    serial print added

--- 03_firmware/03_02_F411/include/sensor_read.h
+++ 03_firmware/03_02_F411/include/sensor_read.h
@@ -7,36 +7,24 @@
 
 void readSensor(SensorType sensorType);
 
-// read data
-// LSM6DS
-extern float lsm6ds_accel_x_read;
-extern float lsm6ds_accel_y_read;
-extern float lsm6ds_accel_z_read;
-extern float lsm6ds_gyro_x_read;
-extern float lsm6ds_gyro_y_read;
-extern float lsm6ds_gyro_z_read;
-extern float lsm6ds_temp_read;
-// DPS310
-extern float dps310_temp_read;
-extern float dps310_press_read;
-// BMI088
-extern float bmi088_accel_x_read;
-extern float bmi088_accel_y_read;
-extern float bmi088_accel_z_read;
-extern float bmi088_gyro_x_read;
-extern float bmi088_gyro_y_read;
-extern float bmi088_gyro_z_read;
-extern float bmi088_temp_read;
-// BMP390
-extern float bmp390_temp_read;
-extern float bmp390_press_read;
+// Sensor readings
+extern float accel_x_read;
+extern float accel_y_read;
+extern float accel_z_read;
+
+extern float gyro_x_read;
+extern float gyro_y_read;
+extern float gyro_z_read;
+
+extern float temp_read;
+extern float press_read;
+
 extern float bmp390_alt_read;
-// LIS2MDL
-extern float lis2mdl_mag_x_read;
-extern float lis2mdl_mag_y_read;
-extern float lis2mdl_mag_z_read;
-// HDC302
-extern float hdc302_hum_read;
-extern float hdc302_temp_read;
+
+extern float mag_x_read;
+extern float mag_y_read;
+extern float mag_z_read;
+
+extern float hum_read;
 
 #endif // SENSOR_READ_R
