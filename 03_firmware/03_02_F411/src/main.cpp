8649d873dc1959389784d41ffca1e9a732b52b8b
Seoyoon Jung <sj4260@nyu.edu>
2025-06-11 23:42:08 -0500

    readFlash returns buffer reading size

--- 03_firmware/03_02_F411/src/main.cpp
+++ 03_firmware/03_02_F411/src/main.cpp
@@ -100,138 +100,135 @@ void LOG_TRIGGER_ISR()
 
     if (pinState == LOW)
     {
-
-        if (pinState == LOW)
-        {
-            // Falling edge detected, start logging in circular
+        // Falling edge detected, start logging in circular
 #if F411_DEBUG_MODE
-            Serial.println("Low pin. Logging in main flash.");
+        Serial.println("Low pin. Logging in main flash.");
 #endif
-            logging_allowed = true;
-            logging_circular = false;
-            neopixel.setPixelColor(0, color_green);
-            neopixel.show();
-        }
-        else if (pinState == HIGH)
-        {
-            // Rising edge detected, start logging in main
-#if F411_DEBUG_MODE
-            Serial.println("High pin. Logging in circular buffer.");
-#endif
-            logging_allowed = true;
-            logging_circular = true;
-            neopixel.setPixelColor(0, color_amber);
-            neopixel.show();
-        }
+        logging_allowed = true;
+        logging_circular = false;
+        neopixel.setPixelColor(0, color_green);
+        neopixel.show();
     }
-
-    void prepareResponse()
+    else if (pinState == HIGH)
     {
-        SPIPacket response(SLAVE_CO1);
-        response.addByte(STATUS_OK);
-        response.addString("DATA");
-        response.serialize(INTERBOARD_TX_BUFFER);
-        coproc_response_size = response.getTotalSize();
+        // Rising edge detected, start logging in main
+#if F411_DEBUG_MODE
+        Serial.println("High pin. Logging in circular buffer.");
+#endif
+        logging_allowed = true;
+        logging_circular = true;
+        neopixel.setPixelColor(0, color_amber);
+        neopixel.show();
     }
+}
 
-    void INTERBOARD_SPI_PROCESS_MSG()
+void prepareResponse()
+{
+    SPIPacket response(SLAVE_CO1);
+    response.addByte(STATUS_OK);
+    response.addString("DATA");
+    response.serialize(INTERBOARD_TX_BUFFER);
+    coproc_response_size = response.getTotalSize();
+}
+
+void INTERBOARD_SPI_PROCESS_MSG()
+{
+    if (first_packet_from_master)
     {
-        if (first_packet_from_master)
-        {
-            // Receive real data, send dummy response
-            HAL_SPI_Receive(hspi, INTERBOARD_RX_BUFFER, MAX_PACKET_SIZE, 300);
+        // Receive real data, send dummy response
+        HAL_SPI_Receive(hspi, INTERBOARD_RX_BUFFER, MAX_PACKET_SIZE, 300);
 
-            master_data_size = INTERBOARD_RX_BUFFER[1];
-            master_message_size = PACKET_HEADER_SIZE + master_data_size + PACKET_CHECKSUM_SIZE;
+        master_data_size = INTERBOARD_RX_BUFFER[1];
+        master_message_size = PACKET_HEADER_SIZE + master_data_size + PACKET_CHECKSUM_SIZE;
 
-            SPIPacket packet;
-            packet.deserialize(INTERBOARD_RX_BUFFER, master_message_size);
+        SPIPacket packet;
+        packet.deserialize(INTERBOARD_RX_BUFFER, master_message_size);
 
 #if F411_DEBUG_MODE
-            Serial.println("Received first packet. Preparing response.");
-            packet.printPacket();
+        Serial.println("Received first packet. Preparing response.");
+        packet.printPacket();
 #endif
-            prepareResponse();
-            first_packet_from_master = false;
+        prepareResponse();
+        first_packet_from_master = false;
+    }
+    else
+    {
+        // Send real response, receive dummy
+        uint8_t response_msg[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
+        uint8_t end_msg[] = {0xBE, 0xEF, 0x00, 0x00, 0x00};
+        uint8_t dummy_rx[MAX_PACKET_SIZE];
+        counter--;
+        if (counter)
+        {
+            HAL_SPI_TransmitReceive(hspi, response_msg, dummy_rx, 5, 100);
+#if F411_DEBUG_MODE
+            _print_buffer("TX", response_msg, 5);
+            _print_buffer("RX", dummy_rx, MAX_PACKET_SIZE);
+#endif
         }
         else
         {
-            // Send real response, receive dummy
-            uint8_t response_msg[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
-            uint8_t end_msg[] = {0xBE, 0xEF, 0x00, 0x00, 0x00};
-            uint8_t dummy_rx[MAX_PACKET_SIZE];
-            counter--;
-            if (counter)
-            {
-                HAL_SPI_TransmitReceive(hspi, response_msg, dummy_rx, 5, 100);
+            HAL_SPI_TransmitReceive(hspi, end_msg, dummy_rx, 5, 100);
+            counter = 5;
+            first_packet_from_master = true;
 #if F411_DEBUG_MODE
-                _print_buffer("TX", response_msg, 5);
-                _print_buffer("RX", dummy_rx, MAX_PACKET_SIZE);
-#endif
-            }
-            else
-            {
-                HAL_SPI_TransmitReceive(hspi, end_msg, dummy_rx, 5, 100);
-                counter = 5;
-                first_packet_from_master = true;
-#if F411_DEBUG_MODE
-                _print_buffer("TX", end_msg, 5);
-                _print_buffer("RX", dummy_rx, MAX_PACKET_SIZE);
+            _print_buffer("TX", end_msg, 5);
+            _print_buffer("RX", dummy_rx, MAX_PACKET_SIZE);
 #endif
 
-                // Set blue to indicate end of transmission
-                neopixel.setPixelColor(0, color_blue);
-                neopixel.show();
-            }
-        }
-
-        INTERBOARD_RCVD_FLAG = false;
-    }
-
-    void INTERBOARD_SPI_ISR()
-    {
-        INTERBOARD_RCVD_FLAG = true;
-    }
-
-    void _print_buffer(const char *label, uint8_t *buffer, uint8_t size)
-    {
-        Serial.print("Packet size: ");
-        Serial.print(size);
-        Serial.print(" | ");
-        Serial.print(label);
-        Serial.print(" Buffer: ");
-        for (int i = 0; i < size; i++)
-        {
-            Serial.print("0x");
-            if (buffer[i] < 16)
-                Serial.print("0");
-            Serial.print(buffer[i], HEX);
-            Serial.print(" ");
-        }
-        Serial.println();
-    }
-
-    void _blink_red()
-    {
-        static unsigned long lastBlinkTime = 0;
-        static bool ledState = false;
-        const unsigned long blinkInterval = 500; // 500ms on/off cycle
-
-        unsigned long currentTime = millis();
-
-        if (currentTime - lastBlinkTime >= blinkInterval)
-        {
-            lastBlinkTime = currentTime;
-            ledState = !ledState;
-
-            if (ledState)
-            {
-                neopixel.setPixelColor(0, color_red);
-            }
-            else
-            {
-                neopixel.clear();
-            }
+            // Set blue to indicate end of transmission
+            neopixel.setPixelColor(0, color_blue);
             neopixel.show();
         }
     }
+
+    INTERBOARD_RCVD_FLAG = false;
+}
+
+void INTERBOARD_SPI_ISR()
+{
+    INTERBOARD_RCVD_FLAG = true;
+}
+
+void _print_buffer(const char *label, uint8_t *buffer, uint8_t size)
+{
+    Serial.print("Packet size: ");
+    Serial.print(size);
+    Serial.print(" | ");
+    Serial.print(label);
+    Serial.print(" Buffer: ");
+    for (int i = 0; i < size; i++)
+    {
+        Serial.print("0x");
+        if (buffer[i] < 16)
+            Serial.print("0");
+        Serial.print(buffer[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+void _blink_red()
+{
+    static unsigned long lastBlinkTime = 0;
+    static bool ledState = false;
+    const unsigned long blinkInterval = 500; // 500ms on/off cycle
+
+    unsigned long currentTime = millis();
+
+    if (currentTime - lastBlinkTime >= blinkInterval)
+    {
+        lastBlinkTime = currentTime;
+        ledState = !ledState;
+
+        if (ledState)
+        {
+            neopixel.setPixelColor(0, color_red);
+        }
+        else
+        {
+            neopixel.clear();
+        }
+        neopixel.show();
+    }
+}
